662fc95c8700669d4ac7bd8f2a09c19b634a358d
phidelis200 <phidelis30@outlook.fr>
2025-04-13 20:41:42 +0300

    Update route.ts

--- src/app/api/contacts/[id]/status/route.ts
+++ src/app/api/contacts/[id]/status/route.ts
@@ -1,9 +1,13 @@
 import { prisma } from "@/lib/prisma";
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-export async function PATCH(request: NextRequest) {
+export async function PATCH(
+  request: Request,
+  { params }: { params: Promise<{ id: string }> }
+) {
   try {
-    const { id, status } = await request.json();
+    const { status } = await request.json();
+    const { id } = await params;
 
     const contact = await prisma.contact.update({
       where: { id: id },
