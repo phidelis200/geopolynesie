2f0a5327987f45b24f6cdcb9844e263db4c42174
phidelis200 <phidelis30@outlook.fr>
2025-04-15 23:54:27 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "export",
-  optimizeCss: true,
+  output: "standalone",
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
   assetPrefix: process.env.NODE_ENV === "production" ? "." : "",
   reactStrictMode: true,
+
+  // facultatif : forcer webpack si Turbopack plante
+  experimental: {
+    turbo: false,
+  },
 };
 
 export default nextConfig;
